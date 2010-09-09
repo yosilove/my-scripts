2817e07d8d3aee6a867a6953d87b85419adf9ad5
yosilove <yosilove@Ryukyu>
2010-09-09 10:52:51 +0000

    [anywhere ]follow buttom

--- twitter-anywhere/my-anywhere.js
+++ twitter-anywhere/my-anywhere.js
@@ -14,7 +14,12 @@ var myTwitter; // Twitter オブジェクト用変数。
 // @Anywhere を初期化。初期化完了後は onAnywhereLoad をコールバック！
 twttr.anywhere(onAnywhereLoad /*, ウインドウコンテキストも指定可能 */);
 
-  // @Anywhere 初期化コールバック。オレ達専用の初期化はここで行う。
+/*followボタンの表示*/
+function showFollow(){
+  myTwitter.('#follow-buttom').followButton("twitterapi");
+}
+
+// @Anywhere 初期化コールバック。オレ達専用の初期化はここで行う。
 function onAnywhereLoad(twitter){
   /*初期設定*/
   myTwitter = twitter; // オレ達用に @Anywhere から授けられた Twitter オブジェクトを保存しておく。
@@ -22,6 +27,7 @@ function onAnywhereLoad(twitter){
   myTwitter.hovercards(); //Twitter IDをみつけると、ユーザ情報を表示する
 
   /*関数呼び出し*/
+  showFollow();
 }
 
 
